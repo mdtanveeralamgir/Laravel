cab54e080c8a20effbe5928ddd8cbb4c336ad572
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-02-22 15:31:07 -0500

    basic of routes

---
+++ exploreLaravel/resources/views/testPage.blade.php
@@ -0,0 +1,3 @@
+<?php
+echo "<p>A new page to display</p>"
+?>
--- exploreLaravel/routes/web.php
+++ exploreLaravel/routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//adding a new page and setting route
+Route::get('/test', function () {
+    return view('testPage');
+});
+
+//returning a text instead of page
+Route::get('/text', function () {
+    return "A simple text";
+});
+
+//passig variables
+Route::get('/var/{value}/{name}', function ($value, $name) {
+    return "Hi! this is {$name}, and value is {$value}";
+});
+
+//give url a short name
+Route::get('/admin/post/example', array('as' => 'admin.home', function () {
+    return route('admin.home');
+}));
