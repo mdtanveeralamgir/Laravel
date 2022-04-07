24ec76b6a198eca3601046907baa71df09815855
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-04-07 13:45:16 -0400

    form login registration authentication

--- exploreLaravel/routes/web.php
+++ exploreLaravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if(Auth::check())
+    {
+        echo "use is logged in";
+    }
+    else
+    {
+        echo "please log in first";
+    }
+//    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
