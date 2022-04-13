6f7ceac4591152e458be58b01dfb15115261f17c
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-04-13 11:17:03 -0400

    email set up and sent using mailgun

--- exploreLaravel/routes/web.php
+++ exploreLaravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+      'title' => 'Hi There',
+      'body' => 'Hope you are enjoying the course',
+    ];
+
+    Mail::send('emails.test', $data, function($message){
+        $message->to('mdtanveeralamgir@gmail.com', 'Tanveer')->subject('From laravel');
+    });
 });
