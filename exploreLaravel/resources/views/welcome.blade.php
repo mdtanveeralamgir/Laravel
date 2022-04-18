e0f579225dc6a3c909c559e2e55d77e9bec33876
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-04-18 11:04:01 -0400

    adding bootstrap and auth

--- exploreLaravel/resources/views/welcome.blade.php
+++ exploreLaravel/resources/views/welcome.blade.php
@@ -130,3 +130,19 @@
         </div>
     </body>
 </html>
+
+
+{{--    <!doctype html>--}}
+{{--<html lang="en">--}}
+{{--<head>--}}
+{{--    <meta charset="UTF-8">--}}
+{{--    <meta name="viewport"--}}
+{{--          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">--}}
+{{--    <meta http-equiv="X-UA-Compatible" content="ie=edge">--}}
+{{--    <link rel="stylesheet" href="{{asset("css/app.css")}}">--}}
+{{--    <title>Document</title>--}}
+{{--</head>--}}
+{{--<body>--}}
+{{--<button class="btn btn-success">bootstrap</button>--}}
+{{--</body>--}}
+{{--</html>--}}
