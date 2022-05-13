9076edcffca925da3854d5b22f45b249425ac40a
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-05-13 11:51:49 -0400

    Setting up accessor and mutator for post_image

--- exploreLaravel/app/Models/Post.php
+++ exploreLaravel/app/Models/Post.php
@@ -4,15 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Post extends Model
 {
     use HasFactory;
 
     protected $guarded= [];
+    protected $appends = ['post_image'];
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
+
+    //setting up mutator (change data before inserting into db)
+    // for post_image column in posts table
+//    public function setPostImageAttribute($value)
+//    {
+//        $this->attributes['post_image'] = asset($value);
+//    }
+
+    //setting up accessor (change data after fetching from db)
+    // for post_image column in posts table
+    public function getPostImageAttribute($value)
+        {
+            if (strpos($value, 'https://') !== FALSE || strpos($value, 'http://') !== FALSE)
+                {
+                return $value;
+                }
+            return asset('storage/' . $value);
+        }
 }
