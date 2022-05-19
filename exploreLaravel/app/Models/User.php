0ce9a67a8dad57d2d0900746d94a8132da84cd2c
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-05-19 12:33:28 -0400

    Adding Role, Permission features. Adding userHasRole() method into model User.php

--- exploreLaravel/app/Models/User.php
+++ exploreLaravel/app/Models/User.php
@@ -46,4 +46,33 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+
+//    public function test()
+//    {
+//        $result = '';
+//        foreach ($this->roles as $role) {
+//            return $role->name ;
+//        }
+//        return $result;
+//    }
+
+    public function userHasRole(...$roleName)
+    {
+        foreach ($this->roles as $role)
+        {
+            if(in_array($role->name, $roleName))
+                return true;
+        }
+
+        return false;
+    }
 }
